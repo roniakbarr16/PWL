cedc2259526c35c11e5de305025123e0505bde6a
Roni Akbar <roniakbar7f@gmail.com>
2026-04-21 22:59:32 +0700

    PWL Codeigniter - Session

---
+++ app/Config/Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('/produk', 'Produkcontroller::index');
+$routes->get('/keranjang', 'Keranjangcontroller::index');
---
+++ app/Views/v_keranjang.php
@@ -0,0 +1,5 @@
+
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+ini isi halaman keranjang
+<?= $this->endSection() ?>
