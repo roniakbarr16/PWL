613f1755885db4871c52890a10e2b18509ff1690
Roni Akbar <roniakbar7f@gmail.com>
2026-04-22 21:53:55 +0700

    PWL Codeigniter - Filter

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends BaseController
             $username = $this->request->getVar('username');
             $password = $this->request->getVar('password');
 
-            $dataUser = ['username' => 'april', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $dataUser = ['username' => 'roni', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
 
             if ($username == $dataUser['username']) {
                 if (md5($password) == $dataUser['password']) {
