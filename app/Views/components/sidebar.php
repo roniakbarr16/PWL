da2a146067f51ce7a5765b21d64c178301dea3b3
Roni Akbar <roniakbar7f@gmail.com>
2026-06-17 14:02:04 +0700

    PWL Codeigniter - Webservice Server

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -28,6 +28,13 @@
         </li><!-- End Produk Nav --> 
 
         <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+                <i class="bi bi-person"></i>
+                <span>History</span>
+            </a>
+        </li><!-- End History Nav -->
+
+        <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
                 <i class="bi bi-person-lines-fill"></i>
                 <span>Profile</span>
